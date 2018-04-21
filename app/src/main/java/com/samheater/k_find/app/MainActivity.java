3aa3329909dca92a63c94d16750302df162a3cd2
PC <yuxchen@clarkson.edu>
2018-04-21 17:32:15 -0400

    FireBase Initial Code

--- app/src/main/java/com/samheater/k_find/app/MainActivity.java
+++ app/src/main/java/com/samheater/k_find/app/MainActivity.java
@@ -9,13 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable()){
+        new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
                 Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
-        }, SPLASH_TIME_OUT);
+        },SPLASH_TIME_OUT);
     }
 }
